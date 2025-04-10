ad7ae2023b7830f83bb66ecfa9a0313a0aa4dd2a
darilin98 <darekrudis@email.cz>
2025-04-11 00:30:34 +0200

    Document player.hpp

--- src/player.hpp
+++ src/player.hpp
@@ -1,6 +1,13 @@
 //
 // Created by Darek Rudiš on 10.02.2025.
 //
+
+/**
+ * @file player.hpp
+ * @author Darek Rudiš
+ * @date 2025-02-10
+ */
+
 #include "rtaudio/RtAudio.h"
 #include "rtaudio/rtaudio_c.h"
 #include "decoder.hpp"
@@ -9,16 +16,37 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
 
+/**
+ * @class Player
+ * @brief Manages playback of data from a Track instance
+ */
 class Player {
 public:
+    /**
+    * @brief Constructs a new Player instance and initializes audio system.
+    */
     explicit Player();
+
+    /**
+     * @brief Redirects local pointer to given Track instance
+     * @param track A valid! Track instance
+     */
     void load_track(const track_ptr_t& track);
+
+    /**
+     * @warning Blocking method
+     * @brief Starts track playback and only completes after stop_track() has been called or Track reached the end
+     */
     void play_track();
     void raise_volume();
     void lower_volume();
     void pause_track();
     void resume_track();
     [[nodiscard]] bool is_paused() const;
+
+    /**
+     * @brief Only way to abort the execution of play_track() method from outside of Player class
+     */
     void stop_track();
     bool is_stopped() const;
 
@@ -30,9 +58,9 @@ private:
     RtAudio audio_;
     RtAudio::StreamParameters params_;
     unsigned int BUFFER_SIZE = 512;
-    unsigned int SAMPLE_RATE = 44100;
-    float saved_volume_ = DEFAULT_VOLUME;
-    track_ptr_t track_;
+    unsigned int SAMPLE_RATE = 44100; ///< Audio sample rate in Hz
+    float saved_volume_ = DEFAULT_VOLUME; ///< Saves volume modifier when switching between tracks
+    track_ptr_t track_; ///< Current Track in playback or ready to be played
 };
 
 #endif //PLAYER_HPP
